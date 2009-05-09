9914c8693586af1c1cf7214fbabb6efa5c1f303c
gwern0 <gwern0@gmail.com>
2009-05-09 14:40:18 +0000

    hlint memo2.hs

--- haskell/memo2.hs
+++ haskell/memo2.hs
@@ -1,10 +1,13 @@
+main :: IO ()
 main = interact (unlines . tabify . couplet . lines)
 
+couplet :: [String] -> [(String, String)]
 couplet [] = []
 couplet (_:[]) = []
 couplet (a:b:c) = [((erase a), b), (a,b)] ++ [(a, (erase b)), (a,b)] ++ couplet c
                   where erase = map (const '_')
 
+tabify :: [(String, String)] -> [String]
 tabify = map (\((x,y), (z,a)) -> x ++ "<br>" ++ y ++ "\t" ++ z ++ "<br>" ++ a) . tuplize
-         where tuplize [] = []
-               tuplize (a:b:c) = [(a,b)] ++ tuplize c
+         where tuplize (a:b:c) = (a,b) : tuplize c
+               tuplize _ = []
