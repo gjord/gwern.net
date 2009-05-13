d45c13c7e153817d6d5f9ada1b922268709dcb08
gwern0 <gwern0@gmail.com>
2009-05-13 03:28:25 +0000

    memo2.hs: cloze delete a little smarter

--- haskell/memo2.hs
+++ haskell/memo2.hs
@@ -5,7 +5,7 @@ couplet :: [String] -> [(String, String)]
 couplet [] = []
 couplet (_:[]) = []
 couplet (a:b:c) = [((erase a), b), (a,b)] ++ [(a, (erase b)), (a,b)] ++ couplet c
-                  where erase = map (const '_')
+                  where erase = map (\x -> if x==' ' then ' ' else '_' )
 
 tabify :: [(String, String)] -> [String]
 tabify = map (\((x,y), (z,a)) -> x ++ "<br>" ++ y ++ "\t" ++ z ++ "<br>" ++ a) . tuplize
