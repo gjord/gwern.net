86c5a55deaf55fb139c029333ea5acaa0c6fe66a
gwern0 <gwern0@gmail.com>
2009-06-02 02:13:06 +0000

    memo.hs: made a slight logic error with the replacements

--- haskell/memo.hs
+++ haskell/memo.hs
@@ -41,7 +41,7 @@ permuteAndPutTogether str = map ((\x -> x ++ "\t" ++ concatQuestn str) . concatQ
  question = [hide "First line", "Second line"]; answer = ["First line",
  "Second line"]. -}
 hide :: String -> String
-hide = map (\x -> if isAlphaNum x then ' ' else x)
+hide = map (\x -> if isAlphaNum x then '_' else x)
 
 {- | A highly general list function exploiting the capabilities of the List
  monad to generate permutations of a list such that we apply a function to all
