b740614660f16baac0cb999d2a5710c1b5a54000
gwern0 <gwern0@gmail.com>
2009-12-14 21:51:47 +0000

    memo2.hs: couldn't handle odd # of lines!

--- haskell/memo2.hs
+++ haskell/memo2.hs
@@ -6,10 +6,14 @@ main :: IO ()
 main = interact (unlines . tabify . couplet . lines)
 
 couplet :: [String] -> [(String, String)]
-couplet [] = []
-couplet (_:[]) = []
-couplet (a:b:c) = [((erase a), b), (a,b)] ++ [(a, (erase b)), (a,b)] ++ couplet c
-                  where erase = map (\x -> if isAlphaNum x then '_' else x )
+couplet (a:b:[]) =  arrange a b
+couplet (a:b:c) = arrange a b ++ couplet (b:c)
+couplet _ = []
+
+arrange :: String -> String -> [(String,String)]
+arrange a b = [((erase a), b), (a,b)] ++ [(a, (erase b)), (a,b)]
+ where erase :: String -> String
+       erase = map (\x -> if isAlphaNum x then '_' else x )
 
 tabify :: [(String, String)] -> [String]
 tabify = map (\((x,y), (z,a)) -> x ++ "<br>" ++ y ++ "\t" ++ z ++ "<br>" ++ a) . tuplize
