004b722da7fba578dd74060e37f6531a780b0432
gwern0 <gwern0@gmail.com>
2011-03-08 16:54:48 +0000

    link-extractor.hs: rm deprecation warning

--- haskell/link-extractor.hs
+++ haskell/link-extractor.hs
@@ -1,11 +1,11 @@
 import System.Environment (getArgs)
-import Text.Pandoc (defaultParserState, processWithM, readMarkdown, Inline(Link), Pandoc)
+import Text.Pandoc (bottomUpM, defaultParserState, readMarkdown, Inline(Link), Pandoc)
 
 main :: IO ()
 main = getArgs >>= mapM readFile >>= mapM_ analyzePage
-          
+
 analyzePage :: String -> IO Pandoc
-analyzePage x = processWithM printLinks (readMarkdown defaultParserState x)
+analyzePage x = bottomUpM printLinks (readMarkdown defaultParserState x)
 
 printLinks :: Inline -> IO Inline
 printLinks (Link _ (x, _)) = putStrLn x >> return undefined
