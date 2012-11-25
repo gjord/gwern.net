7a99b0ab1bccf117ca9870e9e52c6b80c4120d53
gwern0 <gwern0@gmail.com>
2012-11-25 22:13:43 +0000

    hakyll.hs: call out to imageMagick to get <img> dimensions & update HTML with TagSoup script (faster HTML rendering https://groups.google.com/d/topic/pandoc-discuss/c4DwP0yDZog/discussion )

--- hakyll.hs
+++ hakyll.hs
@@ -2,14 +2,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Codec.Binary.UTF8.String (encode)
 import Control.Arrow (arr, (>>>), (>>^))
+import Data.ByteString.Lazy.Char8 (unpack)
 import Data.Char (isAlphaNum, isAscii)
 import Data.FileStore (darcsFileStore)
+import Data.FileStore.Utils (runShellCommand)
+import Data.List (nub, sort)
 import Data.Monoid (mempty, mconcat)
 import Network.HTTP (urlEncode)
 import Network.URI (unEscapeString)
 import System.Directory (removeFile)
+import Text.HTML.TagSoup (renderTagsOptions,parseTags,renderOptions, optMinimize, Tag(TagOpen))
 import Text.Printf (printf)
 import qualified Data.Map as M (fromList, lookup, Map)
+import System.IO.Unsafe (unsafePerformIO)
 
 import Hakyll
 import Feed (filestoreToXmlFeed, FeedConfig(..))
@@ -82,10 +87,10 @@ rssConfig :: FeedConfig
 rssConfig =  FeedConfig { fcTitle = "Joining Clouds", fcBaseUrl  = "http://www.gwern.net", fcFeedDays = 30 }
 
 myPageCompiler :: Compiler Resource (Page String)
-myPageCompiler = cached "myPageCompiler" $ readPageCompiler >>> addDefaultFields >>> arr (changeField "description" escapeHtml) >>> arr applySelf >>> myPageRenderPandocWith
+myPageCompiler = cached "myPageCompiler" (readPageCompiler >>> addDefaultFields >>> arr (changeField "description" escapeHtml) >>> arr applySelf >>> myPageRenderPandocWith)
 
 myPageRenderPandocWith :: Compiler (Page String) (Page String)
-myPageRenderPandocWith = pageReadPandocWith defaultHakyllParserState >>^ fmap pandocTransform >>^ fmap (writePandocWith options)
+myPageRenderPandocWith = pageReadPandocWith defaultHakyllParserState >>^ fmap pandocTransform >>^ fmap (writePandocWith options) >>^ fmap (unsafePerformIO . addImgDimensions)
 
 pandocTransform :: Pandoc -> Pandoc
 pandocTransform = bottomUp (map (convertInterwikiLinks . convertHakyllLinks))
@@ -96,20 +101,45 @@ convertHakyllLinks :: Inline -> Inline
 convertHakyllLinks (Link ref ("", "")) =   let ref' = inlinesToURL ref in Link ref (ref', "Go to wiki page: " ++ ref')
 convertHakyllLinks x = x
 
+-- FASTER HTML RENDERING BY STATICLY SPECIFYING ALL IMAGE DIMENSIONS
+-- read HTML string with TagSoup, process `<img>` tags to read the file's dimensions, and hardwire them
+-- this optimizes HTML rendering since browsers know before downloading the image how to layout the page
+addImgDimensions :: String -> IO String
+addImgDimensions = fmap (renderTagsOptions renderOptions{optMinimize=whitelist}) . mapM staticImg . parseTags
+                 where whitelist s = s /= "div" && s /= "script"
+{- example illustration:
+ TagOpen "img" [("src","/images/201201-201207-traffic-history.png")
+                ("alt","Plot of page-hits (y-axis) versus date (x-axis)")],
+ TagOpen "figcaption" [],TagText "Plot of page-hits (y-axis) versus date (x-axis)",
+ TagClose "figcaption",TagText "\n",TagClose "figure" -}
+staticImg :: Tag String -> IO (Tag String)
+staticImg x@(TagOpen "img" xs) = do let path = lookup "src" xs
+                                    case path of
+                                           Nothing -> return x
+                                           Just p -> do let p' = if head p == '/' then tail p else p
+                                                        (height,width) <- imageMagick p'
+                                                        return (TagOpen "img" (uniq ([("height",height), ("width",width)]++xs)))
+            where uniq = nub . sort
+staticImg x = return x
+-- | Use FileStore util to run imageMagick's 'identify', & extract the dimensions
+imageMagick :: FilePath -> IO (String,String)
+imageMagick f = do (_,_,bs) <- runShellCommand "./" Nothing "identify" ["-format", "%h %w", f]
+                   let [height,width] = words (unpack bs)
+                   return (height,width)
+
+
 -- INTERWIKI PLUGIN
 -- | Derives a URL from a list of Pandoc Inline elements.
 inlinesToURL :: [Inline] -> String
 inlinesToURL x = let x' = inlinesToString x
                      (a,b) = break (=='%') x'
                  in escape a ++ b
-
 -- copied from "XMonad.Actions.Search"
 escape :: String -> String
 escape = concatMap escapeURIChar
          where escapeURIChar :: Char -> String
                escapeURIChar c | isAscii c && isAlphaNum c = [c]
                                | otherwise                 = concatMap (printf "%%%02X") $ encode [c]
-
 
 -- | Convert a list of inlines into a string.
 inlinesToString :: [Inline] -> String
@@ -118,7 +148,6 @@ inlinesToString = concatMap go
                Str s    -> s
                Code _ s -> s
                _        -> " "
-
 convertInterwikiLinks :: Inline -> Inline
 convertInterwikiLinks (Link ref (interwiki, article)) =
   case interwiki of
@@ -130,17 +159,15 @@ convertInterwikiLinks (Link ref (interwiki, article)) =
                 Nothing -> Link ref (interwiki, article)
             where -- 'http://starwars.wikia.com/wiki/Emperor_Palpatine'
                   interwikiurl u a = u ++ urlEncode (deunicode a)
-                  deunicode b = map (\x -> if x == '’' then '\'' else x) b
+                  deunicode = map (\x -> if x == '’' then '\'' else x)
                   -- 'Wookieepedia: Emperor Palpatine'
                   summary a = interwiki' ++ ": " ++ a
     _ -> Link ref (interwiki, article)
 convertInterwikiLinks x = x
-
 -- | Large table of constants; this is a mapping from shortcuts to a URL. The URL can be used by
 --   appending to it the article name (suitably URL-escaped, of course).
 interwikiMap :: M.Map String String
 interwikiMap = M.fromList $ wpInterwikiMap ++ customInterwikiMap
-
 wpInterwikiMap, customInterwikiMap :: [(String, String)]
 customInterwikiMap = [("Hackage", "http://hackage.haskell.org/package/"),
                       ("Hawiki", "http://haskell.org/haskellwiki/"),
