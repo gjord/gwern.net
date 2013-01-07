9d35a6ea8d9a032c1404716da1469ef876106753
gwern0 <gwern0@gmail.com>
2013-01-07 02:10:16 +0000

    link-extractor: +hack to deal with pandoc spinning on hitting hakyll-style metadata

--- haskell/link-extractor.hs
+++ haskell/link-extractor.hs
@@ -5,7 +5,7 @@ main :: IO ()
 main = getArgs >>= mapM readFile >>= mapM_ analyzePage
 
 analyzePage :: String -> IO Pandoc
-analyzePage x = bottomUpM printLinks (readMarkdown defaultParserState x)
+analyzePage x = bottomUpM printLinks (readMarkdown defaultParserState (unlines . drop 1 . lines $ x))
 
 printLinks :: Inline -> IO Inline
 printLinks (Link _ (x, _)) = putStrLn x >> return undefined
