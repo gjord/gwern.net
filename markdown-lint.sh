aaf1fc26a2d7766e7e187e165b961feb96168df9
gwern <gwern@gwern.net>
2013-06-17 23:16:40 +0000

    markdown-lint: force grep highlighting

--- markdown-lint.sh
+++ markdown-lint.sh
@@ -5,29 +5,33 @@ for PAGE in "$@"
 do
     if [[ $PAGE == *.page ]]; then
 
+        function fgp { fgrep --color=always "$@"; }
+        function egp { egrep --color=always "$@"; }
+
         # warn if not text, perhaps due to bad copy-paste
-        cat "$PAGE" | file - | fgrep -v "text";
+        cat "$PAGE" | file - | fgp -v "text";
 
         # find bad URLS, unacceptable domains, malformed syntax, unmatched apostrophes, illegitimate statistics
-        fgrep -e "http://dl.dropbox" -e "http://news.ycombinator.com" -e "http://www.coursera.org" \
+        fgp -e "http://dl.dropbox" -e "http://news.ycombinator.com" -e "http://www.coursera.org" \
               -e ".wiley.com/" -e "http://www.ncbi.nlm.nih.gov/pubmed/" -e "www.tandfonline.com/doi/abs/" \
                  "$PAGE";
-        egrep -e "http://www.pnas.org/content/.*/.*/.*.abstract" "$PAGE";
-        fgrep -e "<q>" -e "</q>" -e "(www" -e ")www" -e "![](" -e " percent " -e "    Pearson'" \
+        egp -e "http://www.pnas.org/content/.*/.*/.*.abstract" "$PAGE";
+        fgp -e "<q>" -e "</q>" -e "(www" -e ")www" -e "![](" -e " percent " -e "    Pearson'" \
               -e '~~~{.sh}' -e ' significant ' -e ' significantly ' "$PAGE";
+        # force no highlighting, because the terminal escape codes trigger bracket-matching
         egrep --only-matching '^\[\^.*\]: ' "$PAGE" | sort | uniq --count | \
-              fgrep --invert-match "      1 [^";
+            fgrep --invert-match "      1 [^";
 
         # image hotlinking deprecated; impolite, and slows page loads & site compiles
-        egrep --only-matching '\!\[.*\]\(http://.*\)' "$PAGE";
+        egp --only-matching '\!\[.*\]\(http://.*\)' "$PAGE";
 
         markdown-length-checker "$PAGE";
         markdown-footnote-length "$PAGE";
 
         declare -r local HTML=`cat "$PAGE" | tail -n +3 | pandoc --mathml --standalone -`
-        echo $HTML | fgrep -e "<""del"">";
+        echo $HTML | fgp -e "<""del"">";
         echo $HTML | elinks -dump --force-html \
-                   | fgrep -e '\frac' -e '\times' -e '(http' -e ')http' -e '[http' -e ']http'  \
+                   | fgp -e '\frac' -e '\times' -e '(http' -e ')http' -e '[http' -e ']http'  \
                            -e ' _ ' -e '[^' -e '^]' -e '<!--' -e '-->' -e '<-- ' -e '<—' -e '—>' \
                            -e '$title$' -e '<del>' \
                            -e '$description$' -e '$author$' -e '$tags$' -e '$category$' \
