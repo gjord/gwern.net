eb22e852298db22d1f6791b2c3da676e860dbcec
gwern <gwern@gwern.net>
2013-06-19 19:26:41 +0000

    lint: most jstor.org links are paywalls, and to be avoided as well

--- markdown-lint.sh
+++ markdown-lint.sh
@@ -13,8 +13,8 @@ do
 
         # find bad URLS, unacceptable domains, malformed syntax, unmatched apostrophes, illegitimate statistics
         fgp -e "http://dl.dropbox" -e "http://news.ycombinator.com" -e "http://www.coursera.org" \
-              -e ".wiley.com/" -e "http://www.ncbi.nlm.nih.gov/pubmed/" -e "www.tandfonline.com/doi/abs/" \
-                 "$PAGE";
+            -e ".wiley.com/" -e "http://www.ncbi.nlm.nih.gov/pubmed/" -e "www.tandfonline.com/doi/abs/" \
+            -e "jstor.org"   "$PAGE";
         egp -e "http://www.pnas.org/content/.*/.*/.*.abstract" "$PAGE";
         fgp -e "<q>" -e "</q>" -e "(www" -e ")www" -e "![](" -e " percent " -e "    Pearson'" \
               -e '~~~{.sh}' -e ' significant ' -e ' significantly ' "$PAGE";
