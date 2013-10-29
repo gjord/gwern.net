9d9adce1676b788f7de6a238a5005a4ed8f6c0b4
gwern <gwern@gwern.net>
2013-10-29 20:16:10 +0000

    markdown-lint: tweak per some of https://github.com/koalaman/shellcheck 's suggestions

--- markdown-lint.sh
+++ markdown-lint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # see http://www.gwern.net/About#markdown-checker
 
+fgp () { fgrep --color=always "$@"; }
+egp () { egrep --color=always "$@"; }
+
 for PAGE in "$@"
 do
     if [[ $PAGE == *.page ]]; then
 
-        function fgp { fgrep --color=always "$@"; }
-        function egp { egrep --color=always "$@"; }
 
         # warn if not text, perhaps due to bad copy-paste
         cat "$PAGE" | file - | fgp -v "text";
@@ -34,10 +35,10 @@ do
         markdown-length-checker "$PAGE";
         markdown-footnote-length "$PAGE";
 
-        declare -r local HTML=`cat "$PAGE" | tail -n +3 | pandoc --mathml --standalone -`
-        echo $HTML | fgp -e "<""del"">";
-        echo $HTML | elinks -dump --force-html \
-                   | fgp -e '\frac' -e '\times' -e '(http' -e ')http' -e '[http' -e ']http'  \
+        declare -r local HTML=$(tail -n +3 "$PAGE" | pandoc --mathml --standalone -)
+        echo "$HTML" | fgp -e "<""del"">";
+        echo "$HTML" | elinks -dump --force-html \
+                     | fgp -e '\frac' -e '\times' -e '(http' -e ')http' -e '[http' -e ']http'  \
                            -e ' _ ' -e '[^' -e '^]' -e '<!--' -e '-->' -e '<-- ' -e '<—' -e '—>' \
                            -e '$title$' -e '<del>' \
                            -e '$description$' -e '$author$' -e '$tags$' -e '$category$' \
