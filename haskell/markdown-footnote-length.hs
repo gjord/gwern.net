bd795264a7473cc3afc200da2af20a534c129bf8
gwern <gwern@gwern.net>
2014-01-11 19:54:07 +0000

    update to work with my latest Pandoc install

--- haskell/markdown-footnote-length.hs
+++ haskell/markdown-footnote-length.hs
@@ -3,11 +3,9 @@
 -- example usage: $ find ~/wiki/ -name "*.page" -exec runhaskell markdown-footnote-length {} \;
 -- default: looks for footnotes >2400 characters long (too long on my screen)
 import System.Environment (getArgs)
-import Text.Pandoc (bottomUpM, defaultParserState, readMarkdown, writeMarkdown,
-                    CiteMethod(Citeproc), HTMLSlideVariant(NoSlides), HTMLMathMethod(PlainMath),
-                    Inline(Note), Meta(..), Pandoc(..), WriterOptions(..))
+import Text.Pandoc (bottomUpM, def, nullMeta, readMarkdown, writeMarkdown,
+                    Inline(Note), Pandoc(..))
 import Control.Monad (void, when)
-import Text.Pandoc.Highlighting (pygments)
 
 main :: IO ()
 main = do (file:_) <- getArgs
@@ -15,51 +13,10 @@ main = do (file:_) <- getArgs
 
 -- 'drop 3' to avoid the near-infinite loop when files start with Hakyll metadata
 processLint :: FilePath -> String -> IO Pandoc
-processLint f x = bottomUpM (footNoteCheck f) (readMarkdown defaultParserState (drop 3 x))
+processLint f x = bottomUpM (footNoteCheck f) (readMarkdown def (drop 3 x))
 
 footNoteCheck :: FilePath -> Inline -> IO Inline
 footNoteCheck f x@(Note cntnts) = do let md = writeMarkdown def (Pandoc nullMeta cntnts)
                                      when (length md > 2400) $ error (f ++ ": " ++ md)
                                      return x
 footNoteCheck _ x = return x
-
-nullMeta :: Meta
-nullMeta = Meta { docTitle = []
-                , docAuthors = []
-                , docDate = [] }
-
-def :: WriterOptions
-def = WriterOptions { writerStandalone         = False
-                      , writerTemplate         = ""
-                      , writerVariables        = []
-                      , writerTabStop          = 4
-                      , writerTableOfContents  = False
-                      , writerSlideVariant     = NoSlides
-                      , writerIncremental      = False
-                      , writerHTMLMathMethod   = PlainMath
-                      , writerIgnoreNotes      = False
-                      , writerNumberSections   = False
-                      , writerSectionDivs      = False
-                      , writerReferenceLinks   = False
-                      , writerWrapText         = True
-                      , writerColumns          = 72
-                      , writerIdentifierPrefix = ""
-                      , writerSourceDirectory  = "."
-                      , writerUserDataDir      = Nothing
-                      , writerCiteMethod       = Citeproc
-                      , writerBiblioFiles      = []
-                      , writerHtml5            = False
-                      , writerBeamer           = False
-                      , writerSlideLevel       = Nothing
-                      , writerChapters         = False
-                      , writerListings         = False
-                      , writerHighlight        = False
-                      , writerSetextHeaders    = True
-                      , writerTeXLigatures     = True
-                      , writerEPUBMetadata     = ""
-                      , writerStrictMarkdown   = True
-                      , writerLiterateHaskell  = False
-                      , writerEmailObfuscation = undefined
-                      , writerHighlightStyle   = pygments
-                      , writerXeTeX = False
-                      }
--- haskell/markdown-length-checker.hs
+++ haskell/markdown-length-checker.hs
@@ -2,7 +2,7 @@
 -- example usage: $ find ~/wiki/ -name "*.page" -exec runhaskell length-warner.hs {} \;
 -- default: looks for lines >110 characters long
 import System.Environment (getArgs)
-import Text.Pandoc (bottomUpM, defaultParserState, readMarkdown, Block(CodeBlock), Pandoc)
+import Text.Pandoc (bottomUpM, def, readMarkdown, Block(CodeBlock), Pandoc)
 import Control.Monad (void, when)
 
 main :: IO ()
@@ -11,7 +11,7 @@ main = do (file:_) <- getArgs
 
 -- 'drop 3' to avoid the near-infinite loop when files start with Hakyll metadata
 processLint :: FilePath -> String -> IO Pandoc
-processLint f x = bottomUpM (lineCheck f) (readMarkdown defaultParserState (drop 3 x))
+processLint f x = bottomUpM (lineCheck f) (readMarkdown def (drop 3 x))
 
 lineCheck :: FilePath -> Block -> IO Block
 lineCheck f x@(CodeBlock _ cntnts) = do mapM_ (\a -> when (length a >= 110)
