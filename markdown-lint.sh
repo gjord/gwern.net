8604dd5dc52bcb3ea9fc7d273cce436b37dfd911
gwern <gwern@gwern.net>
2014-01-18 13:01:51 -0500

    markdown-lint: flag usage of `t.test()` After doing some further reading about non-parametric tests and t.test's assumption of normality, I've decided that I should probably switch in general to a non-parametric two-sample test like `wilcox.test()`: it protects me from non-normality (true in at least two of the pages in which I had used `t.test()` initially) which is not something I ever bother verifying is true, but is still pretty efficient. There are some situations in which the _t_-test is appropriate, like when I am trying to replicate someone's previous work which used a _t_-test (eg. the Ling section in Creatine.page), but they're relatively rare.
    Unfortunately, there is no easy drop-in non-parametric version of `lm()` that I've
    found so far...

--- markdown-lint.sh
+++ markdown-lint.sh
@@ -16,9 +16,9 @@ do
         fgp -e "http://dl.dropbox" -e "http://news.ycombinator.com" -e "http://github.com" \
             -e "http://www.coursera.org" -e ".wiley.com/" -e "http://www.ncbi.nlm.nih.gov/pubmed/" \
             -e "www.tandfonline.com/doi/abs/" -e "jstor.org" -e "springer.com" -e "springerlink.com" "$PAGE";
-        egp -e "http://www.pnas.org/content/.*/.*/.*.abstract" "$PAGE";
+        egp -e "http://www.pnas.org/content/.*/.*/.*.abstract" -e '[^\.]t\.test\(' "$PAGE";
         fgp -e "<q>" -e "</q>" -e "(www" -e ")www" -e "![](" -e " percent " -e "    Pearson'" \
-              -e '~~~{.sh}' -e 'library("' "$PAGE";
+            -e '~~~{.sh}' -e 'library("' "$PAGE";
 
         # look for personal uses of illegitimate statistics, but filter out blockquotes
         fgp -e ' significant ' -e ' significantly ' "$PAGE" | egrep -v '[[:space:]]*>';
