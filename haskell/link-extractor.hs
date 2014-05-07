c7f4de60cf969d1484e9f01d2139e838566f3d99
gwern <gwern@gwern.net>
2014-05-07 17:34:59 -0400

    Markdown link extractor: update for Pandoc changes

--- haskell/link-extractor.hs
+++ haskell/link-extractor.hs
@@ -1,11 +1,11 @@
 import System.Environment (getArgs)
-import Text.Pandoc (bottomUpM, defaultParserState, readMarkdown, Inline(Link), Pandoc)
+import Text.Pandoc (bottomUpM, def, readMarkdown, Inline(Link), Pandoc)
 
 main :: IO ()
 main = getArgs >>= mapM readFile >>= mapM_ analyzePage
 
 analyzePage :: String -> IO Pandoc
-analyzePage x = bottomUpM printLinks (readMarkdown defaultParserState (unlines . drop 1 . lines $ x))
+analyzePage x = bottomUpM printLinks (readMarkdown def (unlines . drop 1 . lines $ x))
 
 printLinks :: Inline -> IO Inline
 printLinks (Link _ (x, _)) = putStrLn x >> return undefined
